44e88bb674ba3d01d7d620bb8a0c736bbf65b337
diquah <57377930+diquah@users.noreply.github.com>
2025-02-20 15:51:37 -0800

    Replace volume reference array with vecs

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,13 @@ pub trait Volume {
     fn max(&self) -> IVec3;
 }
 
-pub struct Octree<'t, const R: usize> {
-    root_cell: &'t mut OctreeCell<'t, R>,
-    arena: Rc<RefCell<ManuallyDropArena<OctreeCell<'t, R>>>>,
+pub struct Octree<'t> {
+    root_cell: &'t mut OctreeCell<'t>,
+    cell_arena: Rc<RefCell<ManuallyDropArena<OctreeCell<'t>>>>,
+    vec_arena: Rc<RefCell<ManuallyDropArena<Vec<&'t dyn Volume>>>>,
 }
 
-impl<'t, const R: usize> Octree<'t, R> {
+impl<'t> Octree<'t> {
     pub fn new<T>(volumes: &'t Vec<&'t dyn Volume>) -> Self {
         let mut min_bound: glam::IVec3 = volumes.iter().fold(IVec3::ZERO, |a, b| a.min(b.min()));
         let mut max_bound: glam::IVec3 = volumes.iter().fold(IVec3::ZERO, |a, b| a.max(b.max()));
@@ -44,35 +45,40 @@ impl<'t, const R: usize> Octree<'t, R> {
             max_bound.z += 1
         }
 
-        let arena = Rc::new(RefCell::new(ManuallyDropArena::new()));
+        let cell_arena = Rc::new(RefCell::new(ManuallyDropArena::new()));
+        let vec_arena = Rc::new(RefCell::new(ManuallyDropArena::new()));
         let root_cell =
-            OctreeCell::build_recursively(arena.clone(), min_bound, max_bound, &volumes);
+            OctreeCell::build_recursively(cell_arena.clone(), vec_arena.clone(), min_bound, max_bound, &volumes);
 
-        Self { root_cell, arena }
+        Self { root_cell, cell_arena, vec_arena }
     }
 }
 
-impl<'t, const R: usize> Drop for Octree<'t, R> {
+impl<'t> Drop for Octree<'t> {
     fn drop(&mut self) {
-        unsafe { self.arena.borrow_mut().manually_drop() };
+        unsafe {
+            self.cell_arena.borrow_mut().manually_drop();
+            self.vec_arena.borrow_mut().manually_drop();
+        };
     }
 }
 
-enum OctreeCell<'c, const R: usize> {
-    FilledCell(FilledOctreeCell<'c, R>),
-    EmptyCell(EmptyOctreeCell<'c, R>),
+enum OctreeCell<'c> {
+    FilledCell(FilledOctreeCell<'c>),
+    EmptyCell(EmptyOctreeCell<'c>),
     EmptyChildlessCell,
 }
 
-impl<'c, const R: usize> OctreeCell<'c, R> {
+impl<'c> OctreeCell<'c> {
     #[rustfmt::skip]
     fn build_recursively(
-        arena: Rc<RefCell<ManuallyDropArena<OctreeCell<'c, R>>>>,
+        cell_arena: Rc<RefCell<ManuallyDropArena<OctreeCell<'c>>>>,
+        vec_arena: Rc<RefCell<ManuallyDropArena<Vec<&'c dyn Volume>>>>,
         min_bound: IVec3,
         max_bound: IVec3,
         volumes: &'c [&'c dyn Volume],
-    ) -> &'c mut OctreeCell<'c, R> {
-        let mut volumes_inside_cell: Vec<Option<&dyn Volume>> = volumes
+    ) -> &'c mut OctreeCell<'c> {
+        let mut volumes_inside_cell: Vec<&dyn Volume> = volumes
             .iter()
             .filter_map(|v| {
                 let bmax = v.max();
@@ -90,10 +96,9 @@ impl<'c, const R: usize> OctreeCell<'c, R> {
                     None
                 }
             })
-            .map(|v| Some(v))
             .collect();
 
-        if volumes_inside_cell.len() >= R {
+        if volumes_inside_cell.len() >= 8 {
             let midpoint = (max_bound + min_bound).div_euclid(IVec3::splat(2));
 
             let mut subregions_min = [IVec3::ZERO; 8];
@@ -106,91 +111,97 @@ impl<'c, const R: usize> OctreeCell<'c, R> {
             }
 
             let a = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[0],
                 subregions_max[0],
                 volumes,
             );
 
             let b = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[1],
                 subregions_max[1],
                 volumes,
             );
 
             let c = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[2],
                 subregions_max[2],
                 volumes,
             );
 
             let d = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[3],
                 subregions_max[3],
                 volumes,
             );
 
             let e = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[4],
                 subregions_max[4],
                 volumes,
             );
 
             let f = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[5],
                 subregions_max[5],
                 volumes,
             );
 
             let g = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[6],
                 subregions_max[6],
                 volumes,
             );
 
             let h = OctreeCell::build_recursively(
-                arena.clone(),
+                cell_arena.clone(),
+                vec_arena.clone(),
                 subregions_min[7],
                 subregions_max[7],
                 volumes,
             );
 
-            arena
+            cell_arena
                 .borrow_mut()
                 .alloc(OctreeCell::EmptyCell(EmptyOctreeCell {
                     a, b, c, d, e, f, g, h
                 }))
         } else if volumes_inside_cell.len() == 0 {
-            arena.borrow_mut().alloc(OctreeCell::EmptyChildlessCell)
+            cell_arena.borrow_mut().alloc(OctreeCell::EmptyChildlessCell)
         } else {
-            volumes_inside_cell.resize_with(R, || None);
-            arena
-                .borrow_mut()
-                .alloc(OctreeCell::FilledCell(FilledOctreeCell(
-                    volumes_inside_cell.try_into().unwrap_or([None; R]),
-                )))
+            let finalized_volumes_in_cell = vec_arena.borrow_mut().alloc(Vec::with_capacity(volumes_inside_cell.len()));
+            finalized_volumes_in_cell.append(&mut volumes_inside_cell);
+
+            cell_arena.borrow_mut().alloc(OctreeCell::FilledCell(FilledOctreeCell(finalized_volumes_in_cell)))
         }
     }
 }
 
-struct EmptyOctreeCell<'c, const R: usize> {
-    a: &'c OctreeCell<'c, R>,
-    b: &'c OctreeCell<'c, R>,
-    c: &'c OctreeCell<'c, R>,
-    d: &'c OctreeCell<'c, R>,
-    e: &'c OctreeCell<'c, R>,
-    f: &'c OctreeCell<'c, R>,
-    g: &'c OctreeCell<'c, R>,
-    h: &'c OctreeCell<'c, R>,
+struct EmptyOctreeCell<'c> {
+    a: &'c OctreeCell<'c>,
+    b: &'c OctreeCell<'c>,
+    c: &'c OctreeCell<'c>,
+    d: &'c OctreeCell<'c>,
+    e: &'c OctreeCell<'c>,
+    f: &'c OctreeCell<'c>,
+    g: &'c OctreeCell<'c>,
+    h: &'c OctreeCell<'c>,
 }
 
-struct FilledOctreeCell<'c, const R: usize>([Option<&'c dyn Volume>; R]);
+struct FilledOctreeCell<'c>(&'c Vec<&'c dyn Volume>);
 
 #[cfg(test)]
 mod tests {
